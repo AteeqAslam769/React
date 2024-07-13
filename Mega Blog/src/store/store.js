484b27d4912a8efb245ebf40700f64613737721b
Ateeq Aslam <110031067+AteeqAslam769@users.noreply.github.com>
2024-07-13 16:57:57 +0500

    Delete Mega Blog directory

--- Mega Blog/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit' 
-import {authreducer} from '../features/authSlice'
-
-const store = configureStore({
-    reducer:authreducer
-})
-
-export default store
